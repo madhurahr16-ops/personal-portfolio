e18d05c332df3c9a3a13db38cbeb16614f94f4c7
madhurahr16-ops <madhurahr16@gmail.com>
2026-09-14 19:26:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/madhurahr16-ops/personal-portfolio.git', 
+base: '/personal-portfolio/',
 })
